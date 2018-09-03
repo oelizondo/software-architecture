81162c98f3ceda69df4e6dbca5ca4c1240645e6d
oelizondo <oscarmarcelo95@gmail.com>
2018-09-02 21:30:03 -0500

    termina programa 1

--- programa-1/src/main.cpp
+++ programa-1/src/main.cpp
@@ -1,40 +1,96 @@
 #include <iostream>
 #include <string>
+#include <list>
+
 using namespace std;
 
-int main(int argc, const char * argv[]) {
-    string S[10],S2[10],Temp[10];
-    int pos,n,iW=0;
-    
-    while(S[iW-1]!="x"){
-        getline(cin,S[iW]);
-        iW++;
-    };
-    for(int iX=0;iX<iW-1;iX++){
-        n=count(S[iX].begin(),S[iX].end(),' ');
-        
-        for(int i=0;i<=n;i++){
-            pos=S[iX].find(' ');
-            S2[i]=S[iX].substr(0,pos);
-            S[iX]= S[iX].substr(pos+1);
-        }
-        for(int i=0;i<=n;i++){
-            for(int i=0;i<=n;i++)
-                Temp[i]="";
-            for(int i=0;i<=n;i++){
-                if(i+1<=n){
-                    Temp[i+1]=S2[i];
-                }else{
-                    Temp[0]=S2[n];
-                }
-            }
-            for(int i=0;i<=n;i++)
-                cout<<Temp[i]<<" ";
-            for(int i=0;i<=n;i++)
-                S2[i]=Temp[i];
-            cout<<endl;
-        }
+class StringShifter {
+  private:
+    list<string> sSentences;
+    list<string> shiftedSentences;
+
+  public:
+  StringShifter(list<string> sentences) {
+    this->sSentences = sentences;
+  }
+  void shift();
+  list<string> split(string sentence, char delimiter);
+  string join(list<string> words);
+  void print();
+};
+
+void StringShifter::shift()
+{
+  for (int i = 0; i < sSentences.size(); i++)
+  {
+    string sentence = sSentences.front();
+    sSentences.pop_front();
+    shiftedSentences.push_back(sentence);
+    list<string> wordsOfSentence = split(sentence, ' ');
+
+
+    for (int j = 0; j < wordsOfSentence.size(); j++)
+    {
+      string firstWord = wordsOfSentence.front();
+      wordsOfSentence.pop_front();
+      wordsOfSentence.push_back(firstWord);
+      string sentence = join(wordsOfSentence);
+      print();
+      shiftedSentences.push_back(sentence);
     }
-    return 0;
+  }
 }
 
+list<string> StringShifter::split(string sentence, char delimiter) {
+  list<string> words;
+  string word = "";
+  for (int i = 0; i < sentence.length(); i++)
+  {
+    if (sentence[i] == ' ') {
+      words.push_back(word);
+      word = "";
+    } else {
+      word += sentence[i];
+    }
+  }
+
+  return words;
+}
+
+string StringShifter::join(list<string> words) {
+  string sentence = "";
+  for (int i = 0; words.size(); i++) {
+    sentence += words.front();
+    sentence += ' ';
+    words.pop_front();
+  }
+
+  return sentence;
+}
+
+void StringShifter::print() {
+  for (int i = 0; i < shiftedSentences.size(); i++)
+  {
+    string sentence = shiftedSentences.front();
+    shiftedSentences.pop_front();
+    cout << sentence << endl;
+  }
+}
+
+int main() {
+    list<string> oraciones;
+    string oracion;
+
+    cout << "Escribe tu oracion:" << endl;
+    cout << "Escribe exit para salir:" << endl;
+
+    while (oracion != "exit") {
+      getline(cin, oracion);
+      oraciones.push_back(oracion + ' ');
+    }
+
+    StringShifter shifter(oraciones);
+    shifter.shift();
+
+    return 0;
+}
